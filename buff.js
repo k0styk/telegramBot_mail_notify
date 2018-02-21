6f0d48ab3273d15865f2d33c4601730bdfbbe55b
Kostya Rimsha <rimshakonstantin@ya.ru>
2018-02-21 23:08:37 +0500

    something

--- buff.js
+++ buff.js
@@ -93,3 +93,12 @@ function processMessage(msg, seqno) {
 //     const {chat} = msg;
 //     bot.sendMessage(chat.id, 'Pong');
 // });
+
+
+
+// firefox
+{/* <div id="pdf">
+  <object width="400" height="500" type="application/pdf" data="/my_pdf.pdf?#zoom=85&scrollbar=0&toolbar=0&navpanes=0" id="pdf_content">
+    <p>Insert your error message here, if the PDF cannot be displayed.</p>
+  </object>
+</div> */}
